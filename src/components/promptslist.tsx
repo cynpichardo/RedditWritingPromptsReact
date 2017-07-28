d6bc9ebba1d8ed4a725361851da01c71c67f239d
Laura Licari <lalicari94@gmail.com>
2017-07-27 21:58:09 -0700

    Wrapping prompts so that they fit in the taskpane

--- src/components/promptslist.tsx
+++ src/components/promptslist.tsx
@@ -16,7 +16,7 @@ export class PromptsList extends React.Component<PromptsListProps, any> {
 
     render() {
         const listItems = this.props.items.map((item, index) => (
-            <li className='ms-ListBasicExample-itemCell' key={index} onClick={this.addPromptToDoc.bind(this, item)} data-is-focusable={ true }>
+            <li className='ms-ListBasicExample-itemCell' key={index} onClick={this.addPromptToDoc.bind(this, item)} data-is-focusable={ true } style={{"wordWrap":"normal","width":"100%"}}>
                 <span className='ms-ListBasicExample-itemName'>{item.author}</span>
                 <p className='ms-font-m ms-fontColor-neutralPrimary'>{item.title}</p>
                 <br/><br/>
@@ -24,7 +24,7 @@ export class PromptsList extends React.Component<PromptsListProps, any> {
         ));
         return (
             <main className='ms-welcome__main'>
-                <ul className='ms-List ms-welcome__features ms-u-slideUpIn10'>
+                <ul className='ms-List ms-welcome__features ms-u-slideUpIn10' style={{"wordWrap":"normal","width":"100%","tableLayout":"fixed"}}>
                     {listItems}
                 </ul>
                 {this.props.children}
